ebb22c12ff5c6b223c1b4661c7ba8add9e21f6af
Grace Lee <igracelee1@gmail.com>
2020-02-16 16:49:39 -0500

    Add switch statements and higher order functions for keydown events

--- index.js
+++ index.js
@@ -3,14 +3,71 @@ let buttons = document.querySelectorAll('button') // array of buttons
 for (let i = 0; i < buttons.length; i++) {
     let currButton = buttons[i]
 
-    // below, first arg is name of event. second arg is name of function we want to invoke on that click event
-    // currButton.addEventListener('click', handleClick)
+    // below, first arg is name of event. second arg is (named or anonymous) function we want to invoke on that click event
 
-    let audio = new Audio('sounds/kick-bass.mp3')
     // writing anonymous function below as second argument
     currButton.addEventListener('click', function() {
-        audio.play()
+        //'this' refers to currButton
+        let buttonInnerHTML = this.innerHTML
+        makeSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
     })
+}
+
+document.addEventListener('keydown', function(event) {
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play()
+            break
+
+        case 'a':
+            let tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play()
+            break
+
+        case 's':
+            let tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play()
+            break
+
+        case 'd':
+            let tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play()
+            break
+
+        case 'j':
+            let snare = new Audio('sounds/snare.mp3')
+            snare.play()
+            break
+
+        case 'k':
+            let crash = new Audio('sounds/crash.mp3')
+            crash.play()
+            break
+
+        case 'l':
+            let kick = new Audio('sounds/kick-bass.mp3')
+            kick.play()
+            break
+
+        default:
+            console.log(buttonInnerHTML)
+    }
+}
+
+function buttonAnimation(currKey) {
+    let activeButton = document.querySelector('.' + currKey)
+    activeButton.classList.add('pressed')
+    setTimeout(function() {
+        activeButton.classList.remove('pressed')
+    }, 100)
 }
 
 /*
